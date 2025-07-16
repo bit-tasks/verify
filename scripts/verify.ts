2af8acd61317ca46571dd6e4531c4ba9dbec700f
Nacho Aldama <sr.drabx@gmail.com>
2025-07-16 10:29:32 +0200

    feat: use CI scripts (#2)
    * feat: use CI scripts
    
    * test: use branch action for tests
    
    * use nightly version
    
    * fix: ci verify command
    
    * fix: node version
    
    * update test ws
    
    * update test-data

--- scripts/verify.ts
+++ scripts/verify.ts
@@ -1,10 +1,18 @@
-import { exec } from "@actions/exec";
+import { exec, getExecOutput } from "@actions/exec";
+import semver from "semver";
 
-const run = async (skipBuild: boolean, wsdir: string) => {
-  await exec("bit status --strict", [], { cwd: wsdir });
-  if (!skipBuild) {
-    await exec("bit build", [], { cwd: wsdir });
+const run = async (wsdir: string) => {
+  const version = await getExecOutput("bit -v", [], { cwd: wsdir });
+
+  // If the version is lower than 1.11.42, throw an error recommending to downgrade the action version to v1
+  // or upgrade Bit to ^1.11.42
+  if (semver.lt(version.stdout.trim(), "1.11.42")) {
+    throw new Error(
+      "Bit version is lower than 1.11.42. Please downgrade the action version to v1, or upgrade Bit to ^1.11.42"
+    );
   }
+
+  await exec("bit ci verify", [], { cwd: wsdir });
 };
 
 export default run;
